7b7c0e336efc40e926f7605ba5f52d2be514c474
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-03-31 11:13:30 +0300

    add input

--- maslov.aleksey/K8/main.cpp
+++ maslov.aleksey/K8/main.cpp
@@ -1,2 +1,38 @@
+#include <iostream>
+#include <function>
+
+template< class T, class Cmp >
+struct BiTree
+{
+  T data;
+  Cmp cmp;
+  BiTree< T, Cmp > * left, * right, * parent;
+};
+
+template< class T, class Cmp >
+BiTree< T, Cmp > * extract(BiTree< T, Cmp > * root, const T & value, BiTree< T, Cmp > ** result);
+
+
 int main()
-{}
+{
+  size_t length = 0;
+  if (!std::cin >> length)
+  {
+    std::cerr << "ERROR: input length\n";
+    return 1;
+  }
+  if (length == 0)
+  {
+    return 0;
+  }
+  int * elements = new int[length];
+  for (size_t i = 0; i < length; i++)
+  {
+    if (!std::cin >> elements[i])
+    {
+      std::cerr << "ERROR: input elements\n";
+      delete[] elements;
+      return 1;
+    }
+  }
+}
