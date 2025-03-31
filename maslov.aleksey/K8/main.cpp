a70bf432c2a096b49850c6b1b1d28c2a35f40010
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-03-31 11:56:45 +0300

    K8

--- maslov.aleksey/K8/main.cpp
+++ maslov.aleksey/K8/main.cpp
@@ -11,16 +11,51 @@ struct BiTree
 
 template< class T, class Cmp >
 BiTree< T, Cmp > * getTree(std::istream & in)
-{}
+{
+  size_t length;
+  if (!(in >> length))
+  {
+    return nullptr;
+  }
+  int * elements = new int[length];
+  for (size_t i = 0; i < length; ++i)
+  {
+    if (!(in >> elements[i]))
+    {
+      return nullptr;
+    }
+  }
+  BiTree< T, Cmp > * root = nullptr;
+  Cmp cmp;
+  for (size_t i = 0; i < length; ++i)
+  {
+    try
+    {
+      BiTree< T, Cmp > * newNode = new BiTree< T, Cmp >{elements[i], cmp, nullptr, nullptr, nullptr};
+    }
+    catch (const std::bad_alloc & e)
+    {}
+  }
+  return root;
+}
 
 template< class T, class Cmp >
 BiTree< T, Cmp > * extract(BiTree< T, Cmp > * root, const T & value, BiTree< T, Cmp > ** result)
-{}
+{
+  BiTree< T, Cmp > * root = nullptr;
+  return root;
+}
 
 template< class T, class Cmp >
 void clear(BiTree< T, Cmp > * root)
-{}
-
+{
+  if (root)
+  {
+    clear(root->left);
+    clear(root->right);
+    delete root;
+  }
+}
 
 int main()
 {
