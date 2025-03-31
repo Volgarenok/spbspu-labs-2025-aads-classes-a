cb87a2efe1dc84dd61c188eaa31d8fd4452c9f50
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-03-31 13:30:44 +0300

    add del

--- finaev.alexey/K8/main.cpp
+++ finaev.alexey/K8/main.cpp
@@ -1,2 +1,32 @@
+#include <iostream>
+#include <functional>
+
+template < class T, class Cmp >
+struct BiTree
+{
+  T data;
+  Cmp cmp;
+  BiTree< T, Cmp > * left, * right, * parent;
+};
+
+template < class T, class Cmp >
+void deleteTree(BiTree< T, Cmp >* root)
+{
+  if (root)
+  {
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;
+  }
+}
+
+template < class T, class Cmp >
+BiTree< T, Cmp >* getTree(std::istream& in)
+{
+   
+}
+
 int main()
-{}
+{
+  
+}
