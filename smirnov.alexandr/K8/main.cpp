2da899974305e4ce660f4eb92a5b650451f923b0
A1exCRE <alsmmm2065@gmail.com>
2025-03-31 12:00:28 +0300

    getTree

--- smirnov.alexandr/K8/main.cpp
+++ smirnov.alexandr/K8/main.cpp
@@ -10,7 +10,61 @@ struct BiTree
 };
 
 template< class T, class Cmp >
-BiTree< T, Cmp > * getTree(std::istream & is);
+BiTree< T, Cmp > * getTree(std::istream & is)
+{
+  size_t size;
+  if (!(is >> size))
+  {
+    return nullptr;
+  }
+  BiTree< T, Cmp > * root = nullptr;
+  for (size_t i = 0; i < size; ++i)
+  {
+    T value;
+    if (!(is >> value))
+    {
+      clear(root);
+      return nullptr;
+    }
+    BiTree< T, Cmp > * current = root;
+    BiTree< T, Cmp > * parent = nullptr;
+    while (current != nullptr)
+    {
+      parent = current;
+      if (current->cmp(value, current->data))
+      {
+        current = current->left;
+      }
+      else
+      {
+        current = current->right;
+      }
+    }
+    BiTree< T, Cmp > * newTree = nullptr;
+    try
+    {
+      newTree = new BiTree< T, Cmp >{value, Cmp(), nullptr, nullptr, parent};
+    }
+    catch (const std::bad_alloc & e)
+    {
+      clear(root);
+      throw;
+    }
+    if (parent == nullptr)
+    {
+      root = newTree;
+    }
+    else if (parent->cmp(value, parent->data))
+    {
+      parent->left = newTree;
+    }
+    else
+    {
+      parent->right = newTree;
+    }
+  }
+  return root;
+}
 
 template< class T, class Cmp >
 BiTree< T, Cmp > * extract(BiTree< T, Cmp > * root, const T & rhs, BiTree< T, Cmp > ** extracted);
@@ -20,8 +74,8 @@ void clear(BiTree< T, Cmp > * root)
 {
   if (root != nullptr)
   {
-    clearBiTree(root->left);
-    clearBiTree(root->right);
+    clear(root->left);
+    clear(root->right);
     delete root;
   }
 }
