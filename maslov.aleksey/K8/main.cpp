c747829d246a91bcc853dd2079ecf70fc4efa78a
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-03-31 12:35:15 +0300

    input done

--- maslov.aleksey/K8/main.cpp
+++ maslov.aleksey/K8/main.cpp
@@ -10,31 +10,48 @@ struct BiTree
 };
 
 template< class T, class Cmp >
-BiTree< T, Cmp > * getTree(std::istream & in)
+BiTree< T, Cmp > * getTree(const T * elements, size_t length, Cmp cmp)
 {
-  size_t length;
-  if (!(in >> length))
+  if (length == 0)
   {
     return nullptr;
   }
-  int * elements = new int[length];
-  for (size_t i = 0; i < length; ++i)
+  BiTree< T, Cmp > * root = new BiTree< T, Cmp >{elements[0], cmp, nullptr, nullptr, nullptr};
+  for (size_t i = 1; i < length; ++i)
   {
-    if (!(in >> elements[i]))
-    {
-      return nullptr;
-    }
-  }
-  BiTree< T, Cmp > * root = nullptr;
-  Cmp cmp;
-  for (size_t i = 0; i < length; ++i)
-  {
+    BiTree< T, Cmp > * newNode = nullptr;
     try
     {
-      BiTree< T, Cmp > * newNode = new BiTree< T, Cmp >{elements[i], cmp, nullptr, nullptr, nullptr};
+      newNode = new BiTree< T, Cmp >{elements[i], cmp, nullptr, nullptr, nullptr};
     }
-    catch (const std::bad_alloc & e)
-    {}
+    catch (const std::bad_alloc &)
+    {
+      clear(root);
+      throw;
+    }
+    BiTree< T, Cmp > * current = root;
+    BiTree< T, Cmp > * parent = nullptr;
+    while (current)
+    {
+      parent = current;
+      if (cmp(elements[i], current->data))
+      {
+        current = current->left;
+      }
+      else
+      {
+        current = current->right;
+      }
+    }
+    newNode->parent = parent;
+    if (cmp(elements[i], parent->data))
+    {
+      parent->left = newNode;
+    }
+    else
+    {
+      parent->right = newNode;
+    }
   }
   return root;
 }
@@ -42,7 +59,6 @@ BiTree< T, Cmp > * getTree(std::istream & in)
 template< class T, class Cmp >
 BiTree< T, Cmp > * extract(BiTree< T, Cmp > * root, const T & value, BiTree< T, Cmp > ** result)
 {
-  BiTree< T, Cmp > * root = nullptr;
   return root;
 }
 
@@ -59,13 +75,33 @@ void clear(BiTree< T, Cmp > * root)
 
 int main()
 {
-  BiTree< int, std::less< int > > * root = getTree< int, std::less< int > >(std::cin);
-  if (!std::cin)
+  size_t length = 0;
+  if (!(std::cin >> length))
   {
-    clear(root);
     std::cerr << "ERROR: input\n";
     return 1;
   }
+  int * elements = new int[length];
+  for (size_t i = 0; i < length; ++i)
+  {
+    if (!(std::cin >> elements[i]))
+    {
+      delete[] elements;
+      std::cerr << "ERROR: input\n";
+      return 1;
+    }
+  }
+  BiTree< int, std::less< int > > * root = nullptr;
+  try
+  {
+    root = getTree(elements, length, std::less< int >());
+  }
+  catch (const std::bad_alloc &)
+  {
+    std::cerr << "ERROR: memory\n";
+    return 1;
+  }
+
   int value = 0;
   while (!(std::cin >> value).eof() && !std::cin.fail())
   {
