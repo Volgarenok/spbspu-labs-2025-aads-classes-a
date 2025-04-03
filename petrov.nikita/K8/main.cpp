f0f2de76e752f048e5d9b5b4a4a705afee492757
Nekich06 <p.nikita.73526@gmail.com>
2025-04-03 19:09:58 +0300

    Changed output for error

--- petrov.nikita/K8/main.cpp
+++ petrov.nikita/K8/main.cpp
@@ -313,8 +313,8 @@ int main()
     }
     else if (!std::cin)
     {
-      outputBiTree(std::cout, root);
-      std::cout << "\n";
+      outputBiTree(std::cerr, root);
+      std::cerr << "\n";
       clearBiTree(root);
       std::cerr << "ERROR: Invalid argument";
       std::cerr << "\n";
